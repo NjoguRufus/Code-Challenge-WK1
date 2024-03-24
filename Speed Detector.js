51ade0d40cc3f4677d9cc8150edbc042f1502a1f
NjoguRufus <rufus.njogu@student.moringaschool.com>
2024-03-24 11:45:43 +0300

    CodeChallenge

--- Speed Detector.js
+++ Speed Detector.js
@@ -1,16 +1,19 @@
-def check_speed(speed):
-    speed_limit = 70
-    demerit_points = 0
+// the user to enter the speed
+const speedLimit = 70;
+const speed = parseInt(prompt("Enter a speed:")); // parse input to integer
+let points;
 
-    if speed <= speed_limit;
-        print{"Ok"}
-    else:
-        demerit_points = (speed - speed_limit) // 5
+//  to test if the speed was within the given range
+if (speed > speedLimit) {
+    // calculates the points penalty if the speed is above the speed limit
+    points = Math.floor((speed - speedLimit) / 5); // round off to get integer points
 
-        if demerit_points > 12;
-            print("License suspended")
-        else:
-            print(f"Points: {demerit_points}")
-# testfunction
-check_speed(80);  #Points; 2
-check_speed(130) # License suspended
+    //to test if points have reached license suspension 
+    if (points < 12) {
+        console.log(points);
+    } else {
+        console.log("License suspended");
+    }
+} else {
+    console.log("OK");
+}
